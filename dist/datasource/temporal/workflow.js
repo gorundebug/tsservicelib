8463103a96ea96b73d03e427806b214e336748c0
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-08-25 20:41:47 +0300

    Refresh Temporal Workflow package output

--- dist/datasource/temporal/workflow.js
+++ dist/datasource/temporal/workflow.js
@@ -1,5 +1,6 @@
 /** Workflow-isolate-safe Temporal API. Keep process and network imports out. */
 export * from "./headers.js";
+export * from "./contracts.js";
 export * from "./workflow-environment.js";
 export * from "./workflows.js";
 export { makeScheduleTrigger, ScheduleBackend } from "../../runtime/schedule.js";
